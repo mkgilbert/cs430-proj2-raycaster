900cb7b45b84afd77e723d85fe985c6bc136c5bb
mike <mike@nau.edu>
2016-09-25 21:45:45 -0700

    added raycast header file and inclusion in raycast.c

---
+++ include/raycast.h
@@ -0,0 +1,14 @@
+#ifndef RAYCAST_H
+#define RAYCAST_H
+#endif
+
+#ifdef RAYCAST_H
+
+/* custom types */
+typedef struct ray_t {
+    double origin[3];
+    double direction[3];
+} ray;
+
+/* functions */
+#endif
--- raycast.c
+++ raycast.c
@@ -5,11 +5,17 @@
 #include <math.h>
 #include "include/json.h"
 #include "include/3dmath.h"
+#include "include/raycast.h"
 
 int main() {
     FILE *json = fopen("test_01_good.json", "rb");
-    //read_json(json);
+    read_json(json);
     //print_objects(objects);
-    example();
+
+    // loop through objects in the scene and do raycasting
+    int i = 0;
+    while (i < MAX_OBJECTS && strlen(objects[i].type) > 0) {
+        ;
+    }
     return 0;
 }
